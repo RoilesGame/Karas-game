8ca2a17f4cd8df6afe5792e9c7d8130512b014a7
MrEndor <mrendorer@gmail.com>
2023-01-23 23:42:27 +0300

    feat(settings): add game settings
    Initial settings for the game

---
+++ settings.py
@@ -0,0 +1,8 @@
+from os import path
+
+CAPTION = "The Fishing"
+SCREEN_SIZE = (800, 550)
+BACKGROUND_COLOR = (30, 40, 50)
+ASSETS_PATH = ".\\assets"
+
+FONT_PATH = path.join(ASSETS_PATH, "fonts\\Fixedsys500c.ttf")
