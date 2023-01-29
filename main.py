d374baf1248986ee72940447248949b1f3a8500a
MrEndor <mrendorer@gmail.com>
2023-01-29 14:54:52 +0300

    feat(main): changed entry point
    changed entry point

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
-import pygame
+from game.control import Control
+from composite_root import (
+    composite_root
+)
+from settings import CAPTION
 
 
-pygame.init()
-
-
-class GameParameters:
-    height = 700
-    width = 500
-    game_name = "Karas' Game"
-
-
-class Game(GameParameters):
-    def start_game(self):
-        pygame.display.set_mode((self.height, self.width))
-        pygame.display.set_caption(self.game_name)
-
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-
-
-game = Game()
-game.start_game()
-
-pygame.quit()
-
-#русик ишак
+if '__main__' == __name__:
+    composite_root.initialize()
+    control = Control(CAPTION)
+    control.state_machine.setup_states(composite_root.states(), "SPLASH")
+    control.main()
